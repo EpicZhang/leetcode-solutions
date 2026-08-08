0292275f2f941f69170c4cca1944df1371fa7a3d
Klaus <125129245+EpicZhang@users.noreply.github.com>
2026-08-09 04:39:28 +0800

    Time: 0 ms (100%), Space: 19.3 MB (19.09%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,12 +1,30 @@
+# Time complexity: O(n^2)
+# Space complexity: O(1)
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         k = len(nums) - nums.count(val)
+#         if nums.count(val) == 0:
+#             return k
+#         else:
+#             for i in range(nums.count(val)):
+#                 nums.remove(val)
+#                 nums.append(val)
+#             return k
+
+# Time complexity: O(n)
+# Space complexity: O(1)
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = len(nums) - nums.count(val)
-        if nums.count(val) == 0:
-            return k
-        else:
-            for i in range(nums.count(val)):
-                nums.remove(val)
-                nums.append(val)
-            return k
+        k = 0
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+        return k
+       
+
+
+
+
         
         
